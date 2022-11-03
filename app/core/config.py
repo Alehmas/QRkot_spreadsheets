9fad9338f44767ee68ced05fb36ebecf92dc86c8
Олег Маслов <msOleg-2006@rambler.ru>
2022-11-03 22:24:53 +0300

    Рабочий проект

--- app/core/config.py
+++ app/core/config.py
@@ -10,17 +10,17 @@ class Settings(BaseSettings):
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = 'FIRST_SUPERUSER_EMAIL'
     first_superuser_password: Optional[str] = 'FIRST_SUPERUSER_PASSWORD'
-    type: Optional[str] = None
-    project_id: Optional[str] = None
-    private_key_id: Optional[str] = None
-    private_key: Optional[str] = None
-    client_email: Optional[str] = None
-    client_id: Optional[str] = None
-    auth_uri: Optional[str] = None
-    token_uri: Optional[str] = None
-    auth_provider_x509_cert_url: Optional[str] = None
-    client_x509_cert_url: Optional[str] = None
-    email: Optional[str] = None
+    type: Optional[str] = 'type'
+    project_id: Optional[str] = 'project_id'
+    private_key_id: Optional[str] = 'private_key_id'
+    private_key: Optional[str] = 'private_key'
+    client_email: Optional[str] = 'client_email'
+    client_id: Optional[str] = 'client_id'
+    auth_uri: Optional[str] = 'auth_uri'
+    token_uri: Optional[str] = 'token_uri'
+    auth_provider_x509_cert_url: Optional[str] = 'auth_provider_x509_cert_url'
+    client_x509_cert_url: Optional[str] = 'client_x509_cert_url'
+    email: Optional[str] = 'email'
 
     class Config:
         env_file = '.env'
